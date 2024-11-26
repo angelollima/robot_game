d38a8a107c780f4a61308bd434072178846aae42
Angelo Lima <angeloantonio442@gmail.com>
2024-11-26 15:15:12 -0300

    Update cliente_robot.py

--- cliente_robot.py
+++ cliente_robot.py
@@ -1,19 +1,34 @@
 import socket
+from time import sleep
 
-# Set up the socket
+# Configuração do socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 2025))
 endereco_servidor = ("127.0.0.1", 2024)
 
-# Send commands
+def pular(altura):
+    for _ in range(altura):
+        mensagem = "controle;up"
+        sock.sendto(mensagem.encode(), endereco_servidor)
+        sleep(0.05)  # Pequena pausa para simular movimento
+
+    for _ in range(altura):
+        mensagem = "controle;down"
+        sock.sendto(mensagem.encode(), endereco_servidor)
+        sleep(0.05)
+
+# Enviar comandos
 try:
     while True:
-        acao = input("Entre com o comando (up, down, left, right) ou para sair (q): ").strip().lower()
+        acao = input("Entre com o comando (up, down, left, right, jump, (jump,right), (jump,left) ou para sair (q): ").strip().lower()
         if acao == 'q':
             break
-        mensagem = f"controle;{acao}"
-        if mensagem:
-            sent = sock.sendto(mensagem.encode(), endereco_servidor)
+
+        if acao == 'jump':
+            pular(10)
+        else:
+            mensagem = f"controle;{acao}"
+            sock.sendto(mensagem.encode(), endereco_servidor)
 finally:
     print("Fechando socket")
     sock.close()
